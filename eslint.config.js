d1dbbc19caaaee24f182361302bf1c88f4ef2c54
Pedro <pedroantoniogoncalves02@gmail.com>
2025-04-22 11:31:06 -0300

    primeiro commit

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import React from 'react';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js, React },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+function App() {
+  return (
+    <>
+      <div>
+        <h1>pedro antoinio</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
